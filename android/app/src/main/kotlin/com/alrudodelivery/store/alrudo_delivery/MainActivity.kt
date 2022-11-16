f7e5be1b02fdd2bca8bc0403867107f94293bafe
Nayana <Nayana@appinsight.tech>
2022-11-16 13:24:30 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/alrudodelivery/store/alrudo_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alrudodelivery.store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/Iconstants.dart
@@ -0,0 +1,11 @@
+class IConstants
+{
+  //static String API_PATH = "https://admin.grocbay.com/api/app-manager/get-functionality/";
+  //static String API_PATH = "https://login.grocbay.com/api/app-manager/get-functionality/";
+  static String API_PATH = "https://login.alrudo.com/api/app-manager/get-functionality/";
+  //static String API_PATH = "https://ssnadmin.srisrinaisargik.com/api/app-manager/get-functionality/";
+
+  static String APP_NAME = "Alrudo Delivery";
+  static bool isOtp = false;
+
+}
